195f40db40b392e3ac874b63b624dd40d358e2db
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-09-21 16:30:36 -0300

    isolamento widget TextFormField na tela de login

--- lib/login.dart
+++ lib/login.dart
@@ -10,51 +10,80 @@ class _LoginState extends State<Login> {
   final String title = "login";
   TextStyle style = TextStyle(fontSize: 20.0);
 
+  final _cLogin = TextEditingController();
+  final _cPassw = TextEditingController();
+  IconData iconField;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-        child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextFormField(
-                    autovalidate: true,
-
-                  ),
-                  SizedBox(height: 15.0),
-                  TextFormField(
-                    obscureText: true ,
-                  ),
-                  SizedBox(height: 45.0),
-
-                  Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(30.0),
-                    color: Colors.indigoAccent,
-                    child: MaterialButton(
-
-                      minWidth: MediaQuery.of(context).size.width,
-                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      onPressed: () {
-                        Navigator.push(
-                            context, MaterialPageRoute(builder: (context) => TabBarHome()));
-                      },
-                        child: Text("Login")
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                ],
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 200),
+              _widgetText(
+                Icons.person,
+                "login",
+                "Digite o usuário",
               ),
-            ),
+              SizedBox(height: 15.0),
+              _widgetText(
+                Icons.vpn_key,
+                "Senha",
+                "Digite a senha",
+              ),
+
+              SizedBox(height: 45.0),
+              Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.indigoAccent,
+                child: MaterialButton(
+                    minWidth: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TabBarHome()));
+                    },
+                    child: Text("Login")),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+            ],
           ),
+        ),
+      ),
     ));
   }
 
+  TextFormField _widgetText(IconData iconField,String label, String hint,{bool password = false, TextEditingController controller}) {
+    return TextFormField(
+              controller: controller,
+              obscureText: password,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+                filled: true,
+                contentPadding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                prefixIcon: Icon(iconField),
+                //iconField,
+                // prefixIcon: Icon(Icons.person),
+              ),
+            );
+  }
 }
 //////////////////////
+//              TextFormField(
+//                obscureText: true,
+//                controller: _cPassw,
+//              ),
