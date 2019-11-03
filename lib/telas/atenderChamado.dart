211a7d57bc2b4ed753d1218db8c9c4743bd98cc8
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-11-03 18:16:01 -0300

    code cleaning

--- lib/telas/atenderChamado.dart
+++ lib/telas/atenderChamado.dart
@@ -1,5 +1,3 @@
-import 'package:app_vai/telas/prioridadeMenu.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 atenderChamado(BuildContext context, index, snapshot) {
