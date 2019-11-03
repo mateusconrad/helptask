61487ccf8815d640fc86990396288279c2bde2dc
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-11-03 15:24:18 -0300

    cards on atendendo/andamento tab

--- lib/tabs/andamento.dart
+++ lib/tabs/andamento.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:app_vai/finalizarChamado.dart';
@@ -10,44 +11,77 @@ class Andamento extends StatefulWidget {
 class _AndamentoState extends State<Andamento> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              //mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const ListTile(
-                  leading: Icon(Icons.album),
-                  title: Text('Teclado Estragado'),
-                  subtitle: Text('Teclado parou de funcionar'),
-                ),
-                ButtonTheme.bar(
-                  child: ButtonBar(
-                    children: <Widget>[
-                      OutlineButton(
-                        child: const Text('Finalizar'),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> FinalizarChamado()));
-                        },
-                      ),
-                      OutlineButton(
-                        child: const Text('Pausar'),
-                        onPressed: () {/* ... */},
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.info),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: StreamBuilder(
+              stream: Firestore.instance
+                  .collection("chamados")
+                  .orderBy("titulo")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.done:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    if (snapshot.data.documents.length == 0) {
+                      return Center(
+                        child: Text(
+                          "Não há dados!",
+                          style:
+                          TextStyle(color: Colors.redAccent, fontSize: 20),
+                        ),
+                      );
+                    }
+
+                    return ListView.builder(
+                        itemCount: snapshot.data.documents.length,
+                        padding: EdgeInsets.only(
+                            top: 5, left: 5, right: 5, bottom: 10),
+                        itemBuilder: (context, index) {
+                          return Card(
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    //snapshot.data.documents[index].documentID.toString()
+                                    // - pega o ID
+                                    title: Text(
+                                        snapshot
+                                            .data.documents[index].data["titulo"],
+                                        style: TextStyle(fontSize: 25)),
+                                    subtitle: Text(snapshot
+                                        .data.documents[index].data["descricao"]
+                                        .toString()),
+                                  ),
+                                  ButtonTheme.bar(
+                                    child: ButtonBar(
+                                      children: <Widget>[
+                                        OutlineButton(
+                                          child: const Text('Finalizar'),
+                                          onPressed: () {},
+                                        ),
+                                        OutlineButton(
+                                          child: const Text('Atender'),
+                                          onPressed: () {/* ... */},
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.info),
+                                          onPressed: () {},
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ));
+                        });
+                }
+              }),
+        ),
+      ],
     );
   }
 }
