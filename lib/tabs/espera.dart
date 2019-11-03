c850e4482446bef40d31d346a12b594e33efbcd9
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-11-03 02:49:56 -0300

    está listando chamados gravados

--- lib/tabs/espera.dart
+++ lib/tabs/espera.dart
@@ -1,3 +1,5 @@
+import 'package:app_vai/netflix/FilmesEditar.dart';
+import 'package:app_vai/tabs/abrir_chamado.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +13,13 @@ class _EsperaState extends State<Espera> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      /*child: Column(
-        children: <Widget>[
-            StreamBuilder (
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: StreamBuilder(
               stream: Firestore.instance
                   .collection("chamados")
+                  .orderBy("titulo")
                   .snapshots(),
               builder: (context, snapshot) {
                 switch (snapshot.connectionState) {
@@ -27,62 +30,60 @@ class _EsperaState extends State<Espera> {
                       child: CircularProgressIndicator(),
                     );
                   default:
-                    if (snapshot.data.documents.lenght == 0) {
+                    if (snapshot.data.documents.length == 0) {
                       return Center(
                         child: Text(
-                          "Não há Chamados",
-                          style: TextStyle(
-                            color: Colors.redAccent,
-                            fontSize: 20,
-                          ),
+                          "Não há dados!",
+                          style:
+                              TextStyle(color: Colors.redAccent, fontSize: 20),
                         ),
                       );
                     }
-                    print('=== data ===: ${snapshot.data}');
+
                     return ListView.builder(
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text(
-                              snapshot.data.documents[index].data["titulo"],
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            subtitle: Text(
-                              snapshot.data.documents[index]
-                                  .data["descricao"]
-                                  .toString()
-                            ),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                OutlineButton(
-                                  child: const Text('Finalizar'),
-                                  onPressed: () {
-                                    /* ... */
-                                  },
+                        itemCount: snapshot.data.documents.length,
+                        padding: EdgeInsets.only(
+                            top: 5, left: 5, right: 5, bottom: 10),
+                        itemBuilder: (context, index) {
+                          return Card(
+                              child: Column(
+                            children: <Widget>[
+                              ListTile(
+                                //snapshot.data.documents[index].documentID.toString()
+                                // - pega o ID
+                                title: Text(
+                                    snapshot
+                                        .data.documents[index].data["titulo"],
+                                    style: TextStyle(fontSize: 25)),
+                                subtitle: Text(snapshot
+                                    .data.documents[index].data["descricao"]
+                                    .toString()),
+                              ),
+                              ButtonTheme.bar(
+                                child: ButtonBar(
+                                  children: <Widget>[
+                                    OutlineButton(
+                                      child: const Text('Finalizar'),
+                                      onPressed: () {},
+                                    ),
+                                    OutlineButton(
+                                      child: const Text('Atender'),
+                                      onPressed: () {/* ... */},
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.info),
+                                      onPressed: () {},
+                                    ),
+                                  ],
                                 ),
-                                OutlineButton(
-                                  child: const Text('Atender'),
-                                  onPressed: () {
-                                    /* ... */
-                                  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.info),
-                                  onPressed: () {},
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    );
+                              ),
+                            ],
+                          ));
+                        });
                 }
-              },
-            ),
-        ],
-      ),*/
+              }),
+        ),
+      ],
     );
   }
 }
