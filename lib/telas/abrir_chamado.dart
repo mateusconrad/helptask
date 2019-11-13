72a0b949643a27cb5439a61f36a9941a8976da25
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-11-12 22:40:16 -0300

    add image picker import

--- lib/telas/abrir_chamado.dart
+++ lib/telas/abrir_chamado.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AbrirChamado extends StatefulWidget {
   final String tipoEdicao;
