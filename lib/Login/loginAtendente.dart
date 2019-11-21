8b80b1f7e06350f0ce8401e8449051961300b161
Mateus <mateus.conrad.a@gmail.com>
2019-11-21 15:30:53 -0300

    pagina loginAtendente

--- lib/Login/loginAtendente.dart
+++ lib/Login/loginAtendente.dart
@@ -1,92 +1,99 @@
-import 'package:app_vai/home.dart';
+import 'package:app_vai/homeAtendente.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
+class LoginAtendente extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _LoginAtendenteState createState() => _LoginAtendenteState();
 }
 
-class _LoginState extends State<Login> {
-  var focusNomeUsuario = new FocusNode();
-  var focusSenhaUsuario = new FocusNode();
-
+class _LoginAtendenteState extends State<LoginAtendente> {
   TextEditingController nomeUsuario = TextEditingController();
   TextEditingController senhaUsuario = TextEditingController();
 
-  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  var focusNomeUsuario = new FocusNode();
+  var focusSenhaUsuario = new FocusNode();
+  GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
+  initState() {
+    final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+    TextEditingController nomeUsuario = TextEditingController();
+    TextEditingController senhaUsuario = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      padding: EdgeInsets.only(top: 60, left: 40, right: 40),
-      child: Form(
-        key: formkey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              validator: (valor) {
-                if (valor.isEmpty) {
-                  FocusScope.of(context).requestFocus(focusNomeUsuario);
-                  return "Informe o nome";
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                  labelText: "Usuário", icon: Icon(Icons.people)),
-              keyboardType: TextInputType.text,
-              controller: nomeUsuario,
-            ),
-            TextFormField(
-              focusNode: focusSenhaUsuario,
-              autofocus: true,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-              validator: (valor) {
-                if (valor.isEmpty) {
-                  FocusScope.of(context).requestFocus(focusSenhaUsuario);
-                  return "Informe a senha";
-                }
-                return null;
-              },
-              decoration:
-                  InputDecoration(labelText: "Senha", icon: Icon(Icons.lock)),
-              keyboardType: TextInputType.text,
-              controller: senhaUsuario,
-            ),
-
-            RaisedButton(
-                child: Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Colors.blue,
-                onPressed: () {
-                  if (formkey.currentState.validate()) {
-                    //fazer verificação no firebase
-                    Firestore.instance
-                        .collection("usuarios")
-                        .where("login", isEqualTo: nomeUsuario.text)
-                        .where("senha", isEqualTo: senhaUsuario.text)
-                        .getDocuments()
-                        .then((QuerySnapshot doc) {
-                      if (doc.documents.length != 0) {
-                        //achou o usuario
-
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => TabBarHome()));
-                      }
-                    });
-                  }
-                }),
-          ],
+      body: Form(
+        key: _loginFormKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, top: 150, right: 10),
+          child: Column(
+            children: <Widget>[
+              Text("Acesso Atendente",),
+              FieldUser(context),
+              FieldSenha(context),
+              RaisedButton(
+                  onPressed: (){
+                    if (_loginFormKey.currentState.validate()) {
+                      FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                          email: nomeUsuario.text,
+                          password: senhaUsuario.text)
+                          .then((currentUser) => Firestore.instance
+                          .collection("users")
+                          .document(currentUser.uid)
+                          .get()
+                          .then((DocumentSnapshot result) =>
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => TabBarHome())))
+                          .catchError((err) => print(err)))
+                          .catchError((err) => print(err));
+                    }
+                  },
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
+  }
+
+  TextFormField FieldSenha(BuildContext context) {
+    return TextFormField(
+      focusNode: focusSenhaUsuario,
+      obscureText: true,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(labelText: "Senha", icon: Icon(Icons.lock)),
+      keyboardType: TextInputType.emailAddress,
+      controller: senhaUsuario,
+      validator: (valor) {
+        if (valor.isEmpty) {
+          FocusScope.of(context).requestFocus(focusSenhaUsuario);
+          return "Informe a senha";
+        }
+        return null;
+      },
+    );
+  }
+
+  TextFormField FieldUser(BuildContext context) {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: "Usuário", icon: Icon(Icons.people)),
+      keyboardType: TextInputType.text,
+      controller: nomeUsuario,
+      autofocus: true,
+      validator: (valor) {
+        if (valor.isEmpty) {
+          FocusScope.of(context).requestFocus(focusNomeUsuario);
+          return "Informe o nome";
+        }
+        return null;
+      },
+    );
   }
 }
