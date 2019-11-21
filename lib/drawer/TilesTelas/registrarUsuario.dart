0d25a60709e58f224874bae568a510183f47f9d6
Mateus Conrad <mateus.conrad.a@gmail.com>
2019-11-21 00:27:33 -0300

    arquivo registrarUsuario

---
+++ lib/drawer/TilesTelas/registrarUsuario.dart
@@ -0,0 +1,18 @@
+//RaisedButton(
+//child: Text(
+//"Registrar-se",
+//style: TextStyle(color: Colors.white),
+//),
+//color: Colors.blue,
+//onPressed: () {
+//if (formkey.currentState.validate()) {
+//Firestore.instance
+//    .collection("usuarios")
+//    .document()
+//    .setData({
+//'login': nomeUsuario.text,
+//'senha': senhaUsuario.text,
+//});
+////setState(() {});
+//}
+//}),
