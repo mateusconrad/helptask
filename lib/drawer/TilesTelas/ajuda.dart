e68ae5f61aa57fe1151ad6ad6ecfd6b72feb9c24
Mateus <mateus.conrad.a@gmail.com>
2019-11-22 17:22:59 -0300

    identacao

--- lib/drawer/TilesTelas/ajuda.dart
+++ lib/drawer/TilesTelas/ajuda.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pdf_render/pdf_render_widgets.dart';
 
 class AjudaPage extends StatefulWidget {
-
   @override
-
   _AjudaPageState createState() => _AjudaPageState();
 }
 
@@ -17,40 +15,43 @@ class _AjudaPageState extends State<AjudaPage> {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: new AppBar(
-            title: const Text('Documentação', style: TextStyle(fontFamily: "Georgia", fontSize: 15, ),),
-            centerTitle: true,
+    return Scaffold(
+        appBar: new AppBar(
+          title: const Text(
+            'Documentação',
+            style: TextStyle(
+              fontFamily: "Georgia",
+              fontSize: 15,
+            ),
           ),
-          backgroundColor: Colors.grey,
-          body: Center(
-              child: PdfDocumentLoader(
-                assetName: 'documents/docs.pdf',
-                documentBuilder: (context, pdfDocument, pageCount) => LayoutBuilder(
-                    builder: (context, constraints) => ListView.builder(
-                        controller: controller,
-                        itemCount: pageCount,
-                        itemBuilder: (context, index) => Container(
-                            margin: EdgeInsets.all(margin),
-                            padding: EdgeInsets.all(padding),
-                            color: Colors.black12,
-                            child: PdfPageView(
-                                pageNumber: index + 1,
-                                calculateSize: (pageWidth, pageHeight, aspectRatio) => Size(constraints.maxWidth - wmargin, (constraints.maxWidth - wmargin) / aspectRatio),
-                                customizer: (context, page, size) => Stack(
-                                  alignment: Alignment.bottomCenter,
-                                  children: <Widget>[
-                                    if (page != null) page,
-                                    //Text('${index + 1}', style: TextStyle(fontSize: 50)) // adding page number on the bottom of rendered page
-                                  ],)
-                            )
-                        )
-                    )
-                ),
-              )
-          )
-      );
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.grey,
+        body: Center(
+            child: PdfDocumentLoader(
+          assetName: 'documents/docs.pdf',
+          documentBuilder: (context, pdfDocument, pageCount) => LayoutBuilder(
+              builder: (context, constraints) => ListView.builder(
+                  controller: controller,
+                  itemCount: pageCount,
+                  itemBuilder: (context, index) => Container(
+                      margin: EdgeInsets.all(margin),
+                      padding: EdgeInsets.all(padding),
+                      color: Colors.black12,
+                      child: PdfPageView(
+                          pageNumber: index + 1,
+                          calculateSize: (pageWidth, pageHeight, aspectRatio) =>
+                              Size(
+                                  constraints.maxWidth - wmargin,
+                                  (constraints.maxWidth - wmargin) /
+                                      aspectRatio),
+                          customizer: (context, page, size) => Stack(
+                                alignment: Alignment.bottomCenter,
+                                children: <Widget>[
+                                  if (page != null) page,
+                                  //Text('${index + 1}', style: TextStyle(fontSize: 50)) // adding page number on the bottom of rendered page
+                                ],
+                              ))))),
+        )));
   }
-
-
 }
