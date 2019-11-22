c98c1ce5c402ecdfeec57f154704430d768e0978
Mateus <mateus.conrad.a@gmail.com>
2019-11-22 17:20:25 -0300

    identação fora do normal

--- lib/telas/showInfo.dart
+++ lib/telas/showInfo.dart
@@ -1,34 +1,40 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-showInfo(BuildContext context, index, snapshot){
+showInfo(BuildContext context, index, snapshot) {
   var resolucao = snapshot.data.documents[index].data["resolucao"].toString();
   var urlImagem = snapshot.data.documents[index].data["urlImagem"].toString();
   var data = snapshot.data.documents[index].data["dataAbertura"].toString();
   var hora = snapshot.data.documents[index].data["horaAbertura"].toString();
 
-
-
   showDialog<void>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(snapshot.data.documents[index].data["titulo"].
-          toString().
-          toUpperCase()),
+          title: Text(snapshot.data.documents[index].data["titulo"]
+              .toString()
+              .toUpperCase()),
           content: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("Classificação: "+snapshot.data.documents[index].data["classificacao"].toString()),
-              Text("Tipo: "+snapshot.data.documents[index].data["tipo"].toString()),
-              Text("Descrição: "+snapshot.data.documents[index].data["descricao"].toString()),
-              Text("Abertura: "+ data + " " + hora),
-              Text("Resolução: " + resolucao,),
-//              Image.network(urlImagem, width: 150,),
+              Text("Classificação: " +
+                  snapshot.data.documents[index].data["classificacao"]
+                      .toString()),
+              Text("Tipo: " +
+                  snapshot.data.documents[index].data["tipo"].toString()),
+              Text("Descrição: " +
+                  snapshot.data.documents[index].data["descricao"].toString()),
+              Text("Abertura: " + data + " " + hora),
+              Text(
+                "Resolução: " + resolucao,
+              ),
+              Image.network(
+                urlImagem,
+                width: 150,
+              ),
             ],
           ),
         );
-      }
-  );
+      });
 }
